07132fe96e833fad84b9d8920299cce5e4c29947
Bonkook Koo <justinaus2k@gmail.com>
2020-10-08 01:42:57 +0900

    remove : 오류 발생 예상 부분 임시 주석 처리

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Follow } from 'react-twitter-widgets'
+// import { Follow } from 'react-twitter-widgets'
 import styles from './Bio.module.scss'
 
 const Bio = ({ config, expanded }) => (
@@ -13,10 +13,10 @@ const Bio = ({ config, expanded }) => (
       Written by <strong>{config.userName}</strong> who lives and works in San
       Francisco building useful things.
       {` `}
-      <Follow
+      {/* <Follow
         username={config.userTwitter}
         options={{ count: expanded ? true : 'none' }}
-      />
+      /> */}
     </p>
   </>
 )
