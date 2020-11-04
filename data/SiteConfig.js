d28c15ee4965af85f13ba5d4739c780bb1f0813b
Bonkook Koo <justinaus2k@gmail.com>
2020-11-05 00:40:14 +0900

    update : site url

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,10 +3,10 @@ const config = {
   siteTitleShort: 'Justinaus Blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: 'Justinaus Blog', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://vigorous-roentgen-b62cdc.netlify.app', // Domain of your website without pathPrefix.
+  siteUrl: 'https://justinaus.ml', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription:
-    'A Gatsby Markdown Blog stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+    'Justinaus Blog.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
   googleAnalyticsID: 'UA-161211056-1', // GA tracking ID.
